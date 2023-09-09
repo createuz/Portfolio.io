3375a933883a8a1349f18341506122e8db4c23fb
createuz <cybersnic@gmail.com>
2023-09-09 13:11:01 +0500

    Additional information was added to the resume

--- user/views.py
+++ user/views.py
@@ -6,29 +6,29 @@ from django.http import JsonResponse
 from root.settings import EMAIL_USERNAME
 
 
-# def contact_view(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         email = request.POST.get('email')
-#         subject = request.POST.get('subject')
-#         message = request.POST.get('message')
-#
-#         from_email = EMAIL_USERNAME
-#         recipient_list = ['recipient@example.com']
-#
-#         try:
-#             send_mail(
-#                 subject,
-#                 f'Name: {name}\nEmail: {email}\nMessage: {message}',
-#                 from_email,
-#                 recipient_list,
-#                 fail_silently=False,
-#             )
-#             return JsonResponse({'message': 'Your message has been sent. Thank you!'})
-#         except Exception as e:
-#             return JsonResponse({'message': 'An error occurred while sending the email.'}, status=500)
-#
-#     return render(request, 'contact.html')
+def contact_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        from_email = EMAIL_USERNAME
+        recipient_list = ['recipient@example.com']
+
+        try:
+            send_mail(
+                subject,
+                f'Name: {name}\nEmail: {email}\nMessage: {message}',
+                from_email,
+                recipient_list,
+                fail_silently=False,
+            )
+            return JsonResponse({'message': 'Your message has been sent. Thank you!'})
+        except Exception as e:
+            return JsonResponse({'message': 'An error occurred while sending the email.'}, status=500)
+
+    return render(request, 'contact.html')
 
 
 def index(request):
